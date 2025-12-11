452d1bf44b08c8f9a4b9d5ecb4035d688e75e4f0
chetana1504 <chetanatemkar2001@gmail.com>
2025-12-11 11:50:54 +0530

    second commit

--- src/exp1.java
+++ src/exp1.java
@@ -1,6 +1,6 @@
 public class exp1 {
     public static void main(String[] args) {
-        int a=8;
+        int a=20;
         int b=2;
         try{
             int c = a/ b;    // it ll throw the exception
